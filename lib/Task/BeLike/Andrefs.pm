7a1d4beb23e3eb16ea3880366ce400e662ce1942
André Santos <andrefs@cpan.org>
2011-11-17 00:10:01 +0000

    v 0.02

--- lib/Task/BeLike/Andrefs.pm
+++ lib/Task/BeLike/Andrefs.pm
@@ -3,22 +3,13 @@ package Task::BeLike::Andrefs;
 use warnings;
 use strict;
 
-=head1 NAME
+our $VERSION = '0.02';
 
-Task::BeLike::Andrefs - The great new Task::BeLike::Andrefs!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
+1;
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
+Task::BeLike::Andrefs - Install the CPAN modules used by andrefs.
 
 Perhaps a little code snippet.
 
@@ -26,27 +17,6 @@ Perhaps a little code snippet.
 
     my $foo = Task::BeLike::Andrefs->new();
     ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
@@ -59,41 +29,6 @@ the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-BeLik
 automatically be notified of progress on your bug as I make changes.
 
 
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Task::BeLike::Andrefs
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Task-BeLike-Andrefs>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Task-BeLike-Andrefs>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Task-BeLike-Andrefs>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Task-BeLike-Andrefs/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
 =head1 LICENSE AND COPYRIGHT
 
 Copyright 2011 Andre Santos.
@@ -104,7 +39,3 @@ by the Free Software Foundation; or the Artistic License.
 
 See http://dev.perl.org/licenses/ for more information.
 
-
-=cut
-
-1; # End of Task::BeLike::Andrefs
