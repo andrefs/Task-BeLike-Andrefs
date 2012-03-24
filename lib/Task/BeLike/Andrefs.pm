8f673fc71365a72ca49c96cfa8fa074d50b20e5d
André Santos <andrefs@cpan.org>
2012-03-24 17:33:15 +0000

    cleaned a lot of stuff with Dist::Zilla

--- lib/Task/BeLike/Andrefs.pm
+++ lib/Task/BeLike/Andrefs.pm
@@ -1,41 +1,8 @@
+use strict;
+use warnings;
 package Task::BeLike::Andrefs;
 
-use warnings;
-use strict;
-
-our $VERSION = '0.04_01';
+# ABSTRACT: For all the AndreFS wanna-bes
 
 1;
-
-=head1 SYNOPSIS
-
-Task::BeLike::Andrefs - Install the CPAN modules used by andrefs.
-
-Perhaps a little code snippet.
-
-    use Task::BeLike::Andrefs;
-
-    my $foo = Task::BeLike::Andrefs->new();
-    ...
-
-=head1 AUTHOR
-
-Andre Santos, C<< <andrefs at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-task-belike-andrefs at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Task-BeLike-Andrefs>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2011 Andre Santos.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
 
